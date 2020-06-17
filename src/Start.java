dfecfab3e3d7dbbf7af430ab30453ae17a5117b2
sTheOz <sztheoz@gmail.com>
2020-06-17 14:48:26 +0200

    Modify Start

--- src/Start.java
+++ src/Start.java
@@ -3,18 +3,9 @@ package src;
 import src.View.*;
 
 public class Start {
-
-    public static void injectData(){
-        System.out.println("================= DATA INJECTION =================");
-    }
-
     public static void main(String[] args){
-        /*DevicesView devicesView = new DevicesView();
-        BorrowView borrowView = new BorrowView();*/
         MenuView menuView = new MenuView();
-        injectData();
         while((menuView).executeMenu( (menuView).printMenu() ) != -1);
-        //(menuView).executeMenu( (menuView).printMenu() );
     }
 	
 }
